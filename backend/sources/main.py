e7059feb73585c5f748eb0138e9323f9429373ab
Theodor Moroianu <theodor.moroianu@gmail.com>
2022-02-10 16:53:10 +0200

    Update openapi.yml w/ Felix

--- backend/sources/main.py
+++ backend/sources/main.py
@@ -3,7 +3,8 @@ Entry point of the backend.
 Starts a FastAPI server.
 """
 
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
 
 app = FastAPI()
@@ -11,6 +12,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return { "status": "OK" }
+
+@app.post("/")
+async def fun(file: List[UploadFile] = File(...)):
+    print(file[0].filename)
+    # f = File(file[0])
+    # with open("file.pdf", "wb") as fout:
+    #     fout.write(file[0])
+    # print(f)
+    # print(file)
+    return { "status": "OK" }
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
