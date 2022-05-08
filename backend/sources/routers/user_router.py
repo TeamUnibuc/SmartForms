c7972ae41ac61444012e0123b78dfd31ae39718c
Theodor Moroianu <theodor.moroianu@gmail.com>
2022-05-08 11:55:11 +0300

    Add users to DB

--- backend/sources/routers/user_router.py
+++ backend/sources/routers/user_router.py
@@ -1,4 +1,5 @@
 from asyncio.log import logger
+import datetime
 from typing import Optional
 from fastapi import APIRouter, Response
 from pydantic import BaseModel
@@ -7,6 +8,9 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 from authlib.integrations.starlette_client import OAuth, OAuthError
 import os
+
+import smart_forms_types
+import database
 
 # authentication logic
 # taken mostly from
@@ -22,8 +26,11 @@ router = APIRouter(
 config = Config("../.env")
 oauth = OAuth(config)
 
+# url with the openid configuration
+# used by Google Oauth
 CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 
+# register the google client for Oauth
 oauth.register(
     name='google',
     server_metadata_url=CONF_URL,
@@ -33,7 +40,10 @@ oauth.register(
 )
 
 @router.get('/')
-async def home(request: Request):
+async def show_login_button(request: Request):
+    """
+    Return a simple HTML page for registering the user.
+    """
     user = request.session.get('user')
     if user is not None:
         email = user['email']
@@ -83,6 +93,25 @@ async def auth(request: Request):
     # Save the user
     request.session['user'] = dict(user)
 
+    # get the user from the database, and create it if not existent
+    # then update the last sign-in date
+    try:
+        user_db = database.get_user_by_email(user["email"])
+        user_db.last_sign_in_date = datetime.datetime.now()
+        database.update_user(user_db, create=False)
+    except:
+        # user doesn't exist, he just created the account
+        user_db = smart_forms_types.User(
+            account_creation_date=datetime.datetime.now(),
+            last_sign_in_date=datetime.datetime.now(),
+            email=user["email"],
+            picture=user["picture"],
+            name=user["name"],
+            given_name=user["given_name"],
+            family_name=user["family_name"]
+        )
+        database.update_user(user_db, create=True)
+    
     # redirect to the url specified by the frontend
     redirect_link = request.session['redirect_link']
     return RedirectResponse(url=redirect_link)
